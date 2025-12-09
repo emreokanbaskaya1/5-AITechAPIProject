e2b94be711a9708973eda1d18f67515bfb242305
EMRE OKAN BAŞKAYA <emreokanbaskaya@gmail.com>
2025-12-09 21:54:55 +0300

    Add CategoryService and update project references
    - Added `CategoryService` class implementing `ICategoryService`
      with methods for category operations. Implemented `TCreateAsync`
      using `Mapster` for DTO to entity mapping.
    - Updated `AITech.Business.csproj` to include `Mapster` package
      and reordered project references for `AITech.DataAccess` and
      `AITech.DTO`.
    - Defined `ICategoryService` interface extending `IGenericService`
      with specific DTO types in `ICategoryService.cs`.

---
+++ AITech.Business/Services/CategoryServices/CategoryService.cs
@@ -0,0 +1,43 @@
+﻿using AITech.DataAccess.Repositories.CategoryRepositories;
+using AITech.DataAccess.UnitOfWorks;
+using AITech.DTO.CategoryDtos;
+using AITech.Entity.Entities;
+using Mapster;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AITech.Business.Services.CategoryServices
+{
+    public class CategoryService(ICategoryRepository _categoryRepository, IUnitOfWork _unitOfWork) : ICategoryService
+    {
+        public async Task TCreateAsync(CreateCategoryDto createDto)
+        {
+            var category = createDto.Adapt<Category>();
+            await _categoryRepository.CreateAsync(category);
+            _unitOfWork.SaveChangesAsync();
+        }
+
+        public Task TDeleteAsync(ResultCategoryDto resultDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ResultCategoryDto>> TGetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResultCategoryDto> TGetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task TUpdateAsync(UpdateCategoryDto updateDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
